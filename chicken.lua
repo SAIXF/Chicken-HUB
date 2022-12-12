a558f7f12cbf03a7adabc143132ea1a75567a640
SAIXF <118271066+SAIXF@users.noreply.github.com>
2022-12-12 17:34:38 +0700

    Update chicken.lua

--- chicken.lua
+++ chicken.lua
@@ -20,11 +20,24 @@ Section:NewToggle("Fast Attack", "to level up faster", function(state)
     end
 end)
 
+local Tab = Window:NewTab("Setting")
+
 Section:NewSlider("Height", "SliderInfo", 10, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
 
-Section:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.F, function()
-	print("You just clicked the bind")
-end)
+for theme, color in pairs(themes) do
+    Section:NewColorPicker(theme, "Change your "..theme, color, function(color3)
+        Library:ChangeColor(theme, color3)
+    end)
+end
+
+local colors = {
+    SchemeColor = Color3.fromRGB(0,255,255),
+    Background = Color3.fromRGB(0, 0, 0),
+    Header = Color3.fromRGB(0, 0, 0),
+    TextColor = Color3.fromRGB(255,255,255),
+    ElementColor = Color3.fromRGB(20, 20, 20)
+}
+
 
